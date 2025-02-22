52603f8e61649b7be951ea42ec898228168c4ce0
ethantrang <ethan.trang5521@gmail.com>
2025-02-22 11:27:03 +1100

    background layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+    <html lang="en" className={`${GeistSans.variable} bg-[var(--bg)]`}>
+      <body className="bg-[var(--bg)]">
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 
-
 export default async function Page() {
   return <div>
-    dawd
   </div>
 }
